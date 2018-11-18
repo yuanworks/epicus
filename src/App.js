d43f40b40d7b303f67767d863134a3a615fb8ce9
YuanHao Chiang <yuanhao.ark@gmail.com>
2018-11-19 07:05:17 +0800

    Add task component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Task from './components/task';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <>
+        <div>
+          <Task>Pay Rent</Task>
+          <Task>Pay Buliding Admin</Task>
+          <Task>Pay dad's Insurance</Task>
+        </div>
+
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload. Mwahaha!
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </>
+
     );
   }
 }
