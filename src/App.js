472bcf6d656404c9256264f22e09cf57b3fb6285
YuanHao Chiang <yuanhao.ark@gmail.com>
2018-11-22 13:16:42 +0800

    Remove default app js dummy content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Task from './components/task';
 import Epic from './components/epic';
 import { MainContainer } from './components/layout/main_container';
@@ -8,34 +6,13 @@ import { MainContainer } from './components/layout/main_container';
 class App extends Component {
   render() {
     return (
-
-      <>
-        <MainContainer>
-          <Epic title="November's Bills">
-            <Task status='to do'>Pay Rent</Task>
-            <Task status='done'>Pay Buliding Admin</Task>
-            <Task>Pay dad's Insurance</Task>
-          </Epic>
-        </MainContainer>
-
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload. Mwahaha!
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </>
-
+      <MainContainer>
+        <Epic title="November's Bills">
+          <Task status='to do'>Pay Rent</Task>
+          <Task status='done'>Pay Buliding Admin</Task>
+          <Task>Pay dad's Insurance</Task>
+        </Epic>
+      </MainContainer>
     );
   }
 }
