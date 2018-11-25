88c97b9afc32a486709e64fb19e0f57f43a2d95c
YuanHao Chiang <yuanhao.ark@gmail.com>
2018-11-25 09:03:48 +0800

    Add store with renderWithRedux

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import ConnectedApp from './App';
+
 import { render } from 'react-testing-library';
 
+// -- redux --
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/reducers';
+
+function renderWithRedux(ui, { initialState, store = createStore(rootReducer, initialState) } = {} ) {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    store
+  }
+}
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -13,17 +26,17 @@ it('renders without crashing', () => {
 describe("Theme buttons", () => {
 
   it ('has a dark mode button', () => {
-    const { getByText } = render(<App />);
+    const { getByText, debug } = renderWithRedux(<ConnectedApp />);
     const darkModeButton = getByText("Dark mode");
     const textDarkModeButton = darkModeButton.textContent;
     expect(textDarkModeButton).toBe("Dark mode");
 
-    // click won't work as the store needs to be mocked
-    //darkModeButton.click();
+    darkModeButton.click();
+    // debug(); <-- this actually shows the dark mode is on!
   });
 
   it ('has a default theme button', () => {
-    const { getByText } = render(<App />);
+    const { getByText } = renderWithRedux(<ConnectedApp />);
     const defaultModeButton = getByText("Default mode");
     expect (defaultModeButton.textContent).toBe("Default mode");
   });
