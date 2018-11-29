8a9f0cbb9b917d5d3a6e5b0556bd39c42efc4bb7
YuanHao Chiang <yuanhao.ark@gmail.com>
2018-11-29 19:18:20 +0800

    Load Immutable Map from local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,20 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import Immutable from 'immutable';
 
 // if this fails, it should fall back to resetting the store instead of failing: (try setting the store to undefined)
-const persistedState = {}; //localStorage.getItem('StoreState') ? JSON.parse(localStorage.getItem('StoreState')) : {};
+const persistedState = localStorage.getItem('StoreState') ? Immutable.fromJS(JSON.parse(localStorage.getItem('StoreState'))) : Map();
 
-const store =  createStore(rootReducer);
-/*createStore(
+const store = createStore(
   rootReducer,
   persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-*/
 
 // TODO: to add throttle
 // save only desired features
-/*
 store.subscribe( () => {
-  localStorage.setItem('StoreState', JSON.stringify(store.getState()));
+  localStorage.setItem('StoreState', JSON.stringify( store.getState().toJS() ));
 });
-*/
 
 export default store;
